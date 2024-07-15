3e57fd200e85310b96a96568dd7bdfd6f22f4dd5
josemlegal <josemlegal97@gmail.com>
2024-07-15 19:31:26 -0400

    Initial commit

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,11 @@
+// console.log("Hello World");
+
+let a = 10;
+let b = 20;
+let c = a + b;
+let d = "Hello ";
+let e = "World";
+
+const saludo = d + e;
+console.log(saludo);
+// console.table({ a, b, c, d, e });
